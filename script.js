9bc14b53039da0e82c9fd5c0c3c5464e61d615b8
frontendAdventure <mgierszanow@yahoo.co.uk>
2022-01-26 20:28:46 +0100

    Password validation complemented with alert

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const password = document.querySelector("#field2");
+const form = document.querySelector('#form1');
+
+const regex = /^(?=.*[^\w])(?=.*\d).{8,}$/;
+
 password.addEventListener('keyup', ()=>{
+    
     password.value = password.value.trim();
 
-    const regex = /^(?=.*[^\w])(?=.*\d).{8,}$/g;
     if(password.value){
         if(password.value.match(regex)){
             password.classList.remove("password-invalid");
@@ -15,4 +19,19 @@ password.addEventListener('keyup', ()=>{
     } else{
         password.classList.remove("password-valid", "password-invalid");
     }
+})
+
+
+form.addEventListener('submit', event => {
+
+    event.preventDefault();
+    event.stopPropagation();
+    password.value = password.value.trim();
+
+    if(!password.value.match(regex)){
+        alert('Za słabe hasło! Hasło musi składać się z co najmniej 8 znaków i zawierać co najmniej jedną cyfrę i jeden znak specjalny.')
+    } else{
+        form.reset();
+    }
+
 })
